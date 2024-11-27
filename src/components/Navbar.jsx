6c1c1a0f3880e9d4be6534a6b6db2254407e405f
Niaziam <niazuddintanim20@gmail.com>
2024-11-27 13:36:21 +0600

    deploy problem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
       </NavbarBrand>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button as={Link} className='bg-transparent border-2 border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white rounded-3xl' href="#" variant="flat" >
+          <Button as={Link} className='bg-transparent border-2 border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white rounded-3xl' href="" variant="flat" >
             Contact
           </Button>
         </NavbarItem>
